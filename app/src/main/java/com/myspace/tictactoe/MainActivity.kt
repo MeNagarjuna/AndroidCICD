c2b70d1fbd536459e65fe8cc6ef3d79f290419d4
MeNagarjuna <polamreddyn@gmail.com>
2025-02-20 16:50:07 +0530

    first commit

--- app/src/main/java/com/myspace/tictactoe/MainActivity.kt
+++ app/src/main/java/com/myspace/tictactoe/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+/*
+* Main Activity*/
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
